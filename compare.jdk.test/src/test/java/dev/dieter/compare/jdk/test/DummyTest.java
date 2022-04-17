68297ba65674e12f0d739d6d580c64a0e9eea1f1
Dieter Mai <maidieter@googlemail.com>
2022-04-17 17:24:24 +0200

    Add test module

---
+++ compare.jdk.test/src/test/java/dev/dieter/compare/jdk/test/DummyTest.java
@@ -0,0 +1,14 @@
+package dev.dieter.compare.jdk.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DummyTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}

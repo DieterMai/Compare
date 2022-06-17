1a6171f36c7d60ce586fb6cae846d13a7d9e4a5b
Dieter Mai <maidieter@googlemail.com>
2022-06-17 09:27:07 +0200

    Add test for complex tree

--- core.test.unit/src/test/java/dev/dietermai/compare/bl/FileTreeBuilderTest.java
+++ core.test.unit/src/test/java/dev/dietermai/compare/bl/FileTreeBuilderTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.dietermai.compare.model.DirectoryRecord;
 import dev.dietermai.compare.model.FileRecord;
 import dev.dietermai.compare.model.FileTree;
 import dev.dietermai.compare.model.ICommonFile;
@@ -59,6 +60,47 @@ class FileTreeBuilderTest {
 
 		FileTree tree = builder.build(Paths.get("foo", "bar"));
 		assertEquals(files, tree.filesOf(tree.root()));
+	}
+	
+	@Test
+	void test_build_deeplyNestedTree(){
+		Path rootPath = Path.of("foo", "bar");
+		
+		// setup expected
+		RootRecord root = RootRecord.of(rootPath);
+		FileRecord fileA = FileRecord.of(root, "a");
+		DirectoryRecord dirB = DirectoryRecord.of(root, "b");
+		DirectoryRecord dirC = DirectoryRecord.of(root, "c");
+		FileRecord fileCA = FileRecord.of(dirC, "ca");
+		DirectoryRecord dirCB = DirectoryRecord.of(dirC, "cb");
+		DirectoryRecord dirCC = DirectoryRecord.of(dirC, "cc");
+		DirectoryRecord dirCBA = DirectoryRecord.of(dirCB, "cba");
+		
+		Set<ICommonFile> filesInRoot = Set.of(fileA, dirB, dirC);
+		Set<ICommonFile> filesInDirC = Set.of(fileCA, dirCB, dirCC);
+		Set<ICommonFile> filesInDirCB = Set.of(dirCBA);
+		
+		// setup mock
+		when(fs.getFiles(root)).thenReturn(filesInRoot);
+		when(fs.getFiles(dirB)).thenReturn(Set.of());
+		when(fs.getFiles(dirC)).thenReturn(filesInDirC);
+		when(fs.getFiles(dirCB)).thenReturn(filesInDirCB);
+		when(fs.getFiles(dirCC)).thenReturn(Set.of());
+		when(fs.getFiles(dirCBA)).thenReturn(Set.of());
+		
+		
+		
+		FileTree tree = builder.build(rootPath);
+		
+		assertEquals(root, tree.root());
+		assertEquals(filesInRoot, tree.filesOf(root));
+		assertEquals(Set.of(), tree.filesOf(dirB));
+		assertEquals(filesInDirC, tree.filesOf(dirC));
+		assertEquals(filesInDirCB,tree.filesOf( dirCB));
+		assertEquals(Set.of(), tree.filesOf(dirCC));
+		assertEquals(Set.of(), tree.filesOf(dirCBA));
+		
+		
 	}
 
 	private Set<ICommonFile> files(IParent parent, String... names) {

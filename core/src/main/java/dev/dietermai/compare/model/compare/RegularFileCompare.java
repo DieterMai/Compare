10c69427baec57145f15bd07a3893a74a7dcd103
Dieter Mai <maidieter@googlemail.com>
2022-06-22 08:07:11 +0200

    First implementation of RegularFileCompare

--- core/src/main/java/dev/dietermai/compare/model/compare/RegularFileCompare.java
+++ core/src/main/java/dev/dietermai/compare/model/compare/RegularFileCompare.java
@@ -1,5 +1,14 @@
 package dev.dietermai.compare.model.compare;
 
-public record RegularFileCompare() {
+import dev.dietermai.compare.model.file.RegularFile;
+import java.util.List;
+
+public record RegularFileCompare(String name, List<RegularFile> files) {
+	public boolean existense() {
+		return files.stream().allMatch(f -> f != null);
+	}
 	
+	public boolean perfect() {
+		return existense();
+	}
 }
